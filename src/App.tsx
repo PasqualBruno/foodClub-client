3a3bd519715b250ac25c957b7c02db269db92b0f
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2025-06-17 23:58:17 -0300

    fix: propriedades não utilizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import '@ant-design/v5-patch-for-react-19';
-import { useRestaurantStore } from './Entities/Restaurant/store/RestaurantStore';
-import { useEmployeeStore } from './Entities/Employee/store/EmployeeStore';
-import { useCompanyStore } from './Entities/Company/store/CompanyStore';
 
 const App = () => {
 
-  const { restaurant } = useRestaurantStore();
-  const { employee } = useEmployeeStore();
-  const { company } = useCompanyStore();
 
 
   return (
