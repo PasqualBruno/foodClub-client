0067db4273ca6d5f6d26b0c2792db3365526fa3b
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2025-06-21 05:01:37 -0300

    feat: tabela de pedidos das empresas nos restaurantes e simulação do avanço do pedido.

--- src/Entities/Restaurant/repository/restaurantRepository.ts
+++ src/Entities/Restaurant/repository/restaurantRepository.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
 import type { IDish } from "../interfaces/RestaurantInterfaces"
+import { mockCompanyOrders } from "../store/mockData"
+import type { ICompanyOrder, IEmployeeOrder } from "@/Entities/Company/interfaces/CompanyInterfaces"
 
 const apiUrl = import.meta.env.VITE_API_URL
 
@@ -43,6 +45,55 @@ const restaurantRepository = {
     }
   },
 
+  getCompanyOrders: async (restaurantId: number) => {
+    //TODO -- Integrar com o back
+    try {
+      // const response = await axios.get(`${apiUrl}/Order/by-restaurant/${restaurantId}`)
+      // return response.data
+      return mockCompanyOrders
+
+    } catch (error) {
+      console.error('Erro ao buscar pedidos:', error)
+      throw error
+    }
+  },
+
+  updateEmployeeOrder: async (orderId: number, data: Partial<IEmployeeOrder>) => {
+    //TODO -- Integrar com o back
+    try {
+      const updatedOrders = mockCompanyOrders.map((companyOrder) => {
+        // Atualiza employeeOrders dentro do pedido da empresa
+        const updatedEmployeeOrders = companyOrder.employeeOrders.map((employeeOrder) =>
+          employeeOrder.id === orderId ? { ...employeeOrder, ...data } : employeeOrder
+        )
+        return { ...companyOrder, employeeOrders: updatedEmployeeOrders }
+      })
+
+      return updatedOrders
+    } catch (error) {
+      console.error('Erro ao atualizar pedido:', error)
+      throw error
+    }
+  },
+  updateCompanyOrder: async (orderId: number, data: Partial<ICompanyOrder>) => {
+    //TODO -- Integrar com o back
+    try {
+      const updatedOrders = mockCompanyOrders.map((companyOrder) =>
+        companyOrder.id === orderId
+          ? { ...companyOrder, ...data }
+          : companyOrder
+      )
+
+      return updatedOrders
+    } catch (error) {
+      console.error('Erro ao atualizar pedido da empresa:', error)
+      throw error
+    }
+  }
+
+
+
 }
 
 export default restaurantRepository
+
