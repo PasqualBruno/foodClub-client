e29e6e9529253ebfccec1a7ef24936c4c27911c4
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2025-06-26 14:35:10 -0300

    feat: Pedidos recorrentes para os funcionários com base no restaurante escolhido pela empresa

--- src/Entities/Employee/store/EmployeeStore.tsx
+++ src/Entities/Employee/store/EmployeeStore.tsx
@@ -6,26 +6,29 @@ import employeeRepository from "@/Entities/Employee/repository/employeeRepositor
 interface IEmployeeStore {
   employee: IEmployee | null;
   companyEmployees: IEmployee[] | null;
+  employeeWeeklyOrders: IWeeklyOrder[] | null;
   setEmployee: (employee: IEmployee) => void;
   getEmployeeByCompany: (companyId: number) => Promise<IEmployee[]>;
   deleteEmployee: (id: number) => Promise<void>;
   updateEmployee: (id: number, data: Partial<IEmployee>) => Promise<IEmployee>;
   createEmployee: (data: Partial<IEmployee>, companyId: number) => Promise<IEmployee>;
   createWeeklyOrder: (data: IWeeklyOrder) => Promise<IWeeklyOrder>;
+  getWeeklyOrdersByEmployee: (id: number) => Promise<IWeeklyOrder[]>
 }
 
-export const useEmployeeStore = create<IEmployeeStore>((set) => ({
+export const useEmployeeStore = create<IEmployeeStore>((set, get) => ({
   employee: null,
   companyEmployees: null,
+  employeeWeeklyOrders: null,
 
   setEmployee: (employee) => set({ employee }),
+  setEmployeeWeeklyOrders: (orders: IWeeklyOrder[]) => set({ employeeWeeklyOrders: orders }),
 
   getEmployeeByCompany: async (companyId: number) => {
     try {
       const response = await employeeRepository.getEmployeeByCompanyIdTemp(companyId);
-      console.log(response);
       set({ companyEmployees: response });
-      return response; // agora está retornando corretamente um IEmployee[]
+      return response;
     } catch (error) {
       console.error("Erro ao buscar funcionários da empresa:", error);
       throw error;
@@ -35,10 +38,9 @@ export const useEmployeeStore = create<IEmployeeStore>((set) => ({
   deleteEmployee: async (id: number) => {
     try {
       const response = await employeeRepository.deleteEmployee(id);
-      console.log(response);
       return response;
     } catch (error) {
-      console.error("Erro ao excluir funcionário:", error);
+      console.error("Erro ao excluir funcionário:", error);
       throw error;
     }
   },
@@ -46,32 +48,44 @@ export const useEmployeeStore = create<IEmployeeStore>((set) => ({
   updateEmployee: async (id: number, data: Partial<IEmployee>) => {
     try {
       const response = await employeeRepository.updateEmployee(id, data);
-      console.log(response);
       return response;
     } catch (error) {
-      console.error("Erro ao atualizar funcionário:", error);
+      console.error("Erro ao atualizar funcionário:", error);
       throw error;
     }
   },
 
-  createEmployee: async (data: Partial<IEmployee>) => {
+  createEmployee: async (data: Partial<IEmployee>, companyId: number) => {
     try {
-      const response = await employeeRepository.createEmployee(data, data.companyId!);
-      console.log(response);
+      const response = await employeeRepository.createEmployee(data, companyId);
       return response;
     } catch (error) {
-      console.error("Erro ao criar funcionário:", error);
+      console.error("Erro ao criar funcionário:", error);
       throw error;
     }
   },
+
   createWeeklyOrder: async (data: IWeeklyOrder) => {
     try {
       const response = await employeeRepository.createWeeklyOrder(data);
-      console.log(response);
+      // Após criar o pedido, chame a função de busca para atualizar a store
+      const { getWeeklyOrdersByEmployee } = get();
+      await getWeeklyOrdersByEmployee(data.employeeId);
       return response;
     } catch (error) {
-      console.error("Erro ao criar funcionário:", error);
+      console.error("Erro ao criar funcionário:", error);
       throw error;
     }
-  }
+  },
+
+  getWeeklyOrdersByEmployee: async (id: number) => {
+    try {
+      const response = await employeeRepository.getWeeklyOrdersByEmployee(id);
+      set({ employeeWeeklyOrders: response });
+      return response;
+    } catch (error) {
+      console.error("Erro ao buscar pedidos semanais do funcionário:", error);
+      throw error;
+    }
+  },
 }));
